4bfe157f650176b1314e3ca8c4f7df39dbdfd9fd
pragadishwaran <pragadish1995@gmail.com>
2019-09-03 19:25:29 +0530

    first check

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './src/store';
-// import FirstComponent from './src/components/FirstComponent';
+import FirstComponent from './src/components/FirstComponent';
 import { LoginScreen } from './src/Screens';
 
 
